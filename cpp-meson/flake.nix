78fe7f336c8359dfdd7c5333def92c41269bcf3e
lily <contact@lilyvex.dev>
2025-05-21 23:59:41 -0400

    Initial commit

---
+++ cpp-meson/flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "cpp-meson-flake";
+          version = "0.1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [ meson ninja pkg-config ];
+          buildInputs = with pkgs; [ llvmPackages_20.libcxxClang ];
+
+          # Optional: for debugging
+          hardeningDisable = [ "all" ];
+
+          # Meson expects build inside a separate dir
+          configurePhase = ''
+            meson setup builddir
+          '';
+
+          buildPhase = ''
+            cd builddir
+            meson compile
+          '';
+
+          installPhase = ''
+            meson install --destdir=$out
+          '';
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ meson ninja pkg-config ];
+          buildInputs = with pkgs; [ llvmPackages_20.libcxxClang ];
+          shellHook = ''
+            export CC=clang
+            export CXX=clang++
+          '';
+        };
+      }
+    );
+}
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Lily's flake templates";
+
+  outputs = { self }: {
+    templates = {
+      rust = {
+        path = ./rust;
+        description = "Rust template, using Naersk";
+      };
+
+      go = {
+        path = ./go;
+        description = "Go template";
+      };
+
+      cpp-meson = {
+        path = ./cpp-meson;
+        description = "C++ Meson template with Clang";
+      };
+
+      defaultTemplate = self.templates.rust;
+    };
+  };
+}
